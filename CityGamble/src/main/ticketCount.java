16dbeab13672723197e6fc3c1bcd9ce4a2b0e395
AshourAziz <ashazi1@nths219.org>
2016-01-20 22:09:51 -0600

    Moving to CityAPI.

--- CityGamble/src/main/ticketCount.java
+++ CityGamble/src/main/ticketCount.java
@@ -17,36 +17,23 @@ public class ticketCount {
 
 	public static void Charge(String player, int amount) {
 
-		// let's charge the player for a red ticket. given to us when calling
-		// "charge"
-		EconomyResponse r = Main.econ.bankWithdraw(player, amount);
-		String amountString = Integer.toString(amount);
+		// ChatColor.DARK_GRAY + "[" + ChatColor.RED + ChatColor.BOLD + "Gamble"
+		// + ChatColor.DARK_GRAY + "]" + ChatColor.AQUA + "You've purchased a "
+		// + ChatColor.RED + "Red"
+		// + ChatColor.AQUA + " gamble ticket, cost of $" + amountString + "
+		// GOOD LUCK!"
 
-		if (r.transactionSuccess()) {
-			Bukkit.getPlayer(player).sendMessage(
-					ChatColor.DARK_GRAY + "[" + ChatColor.RED + ChatColor.BOLD
-							+ "Gamble" + ChatColor.DARK_GRAY + "]"
-							+ ChatColor.AQUA + "You've purchased a "
-							+ ChatColor.RED + "Red" + ChatColor.AQUA
-							+ " gamble ticket, cost of $" + amountString
-							+ " GOOD LUCK!");
-		} else {
-			Bukkit.getPlayer(player).sendMessage(""
-					+ ChatColor.DARK_RED
-					+ ChatColor.BOLD
-					+ "ERROR OCCURED while purching a RED Ticket, please contact staff with a screenshot of this message!");
-		}
+		CityAPI.Economy.charge(player, amount, "replaceme",
+				"&4&lERROR OCCURED while purching a RED Ticket, please contact staff with a screenshot of this message!");
+		
 
 	}
 
 	public static void addTicketValue(String player, String color) {
 		// TODO: We need to move this to it's own class.
-		String ticketRed = Main.playerTickets.get(Bukkit.getPlayer(player))
-				.split("-")[0];
-		String ticketBlack = Main.playerTickets.get(Bukkit.getPlayer(player))
-				.split("-")[1];
-		String ticketGreen = Main.playerTickets.get(Bukkit.getPlayer(player))
-				.split("-")[2];
+		String ticketRed = Main.playerTickets.get(Bukkit.getPlayer(player)).split("-")[0];
+		String ticketBlack = Main.playerTickets.get(Bukkit.getPlayer(player)).split("-")[1];
+		String ticketGreen = Main.playerTickets.get(Bukkit.getPlayer(player)).split("-")[2];
 
 		if (color == "red") {
 			// Let's parse the red ticket count into an int
@@ -57,8 +44,7 @@ public class ticketCount {
 			// We're going to transfer it back into a string
 			ticketRed = Integer.toString(redcount);
 			// recompile the amount of tickets this player has
-			String recompile = ticketRed + "-" + ticketBlack + "-"
-					+ ticketGreen;
+			String recompile = ticketRed + "-" + ticketBlack + "-" + ticketGreen;
 			// we're puting to put him back into the hashmap
 			Main.playerTickets.put(player, recompile);
 
@@ -66,15 +52,13 @@ public class ticketCount {
 			int blackcount = Integer.parseInt(ticketBlack);
 			blackcount++;
 			ticketBlack = Integer.toString(blackcount);
-			String recompile = ticketRed + "-" + ticketBlack + "-"
-					+ ticketGreen;
+			String recompile = ticketRed + "-" + ticketBlack + "-" + ticketGreen;
 			Main.playerTickets.put(player, recompile);
 		} else if (color == "green") {
 			int greencount = Integer.parseInt(ticketGreen);
 			greencount++;
 			ticketGreen = Integer.toString(greencount);
-			String recompile = ticketRed + "-" + ticketBlack + "-"
-					+ ticketGreen;
+			String recompile = ticketRed + "-" + ticketBlack + "-" + ticketGreen;
 			Main.playerTickets.put(player, recompile);
 		}
 	}
