9a54143d90470ca0e3dd0985145ff02332f9b87a
Backup <Backup@Backup-PC>
2016-01-22 12:07:35 -0600

    Finished, added an API, cache clearing system essentially everything. Entering testing phase.

--- CityGamble/src/main/API.java
+++ CityGamble/src/main/API.java
@@ -4,89 +4,134 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.*;
-
-
 import java.util.HashMap;
 
 public class API {
 
-    protected static int broadcastCounter = 0;
+	protected static int broadcastCounter = 0;
+	static String cachedColor = "";;
 
-    public static HashMap<String, String> getTicketHash() {
-        return Main.playerTickets;
-    }
+	public static HashMap<String, String> getTicketHash() {
+		return Main.playerTickets;
+	}
 
-    public static int ticketsSold() {
+	public static int ticketsSold() {
 
-        //We should create an array list, and just keep adding one and return the size of the array list.
-        int sold = getTicketHash().size();
-        return sold;
-    }
+		// We should create an array list, and just keep adding one and return
+		// the size of the array list.
+		int sold = getTicketHash().size();
+		return sold;
+	}
 
-    public static Inventory myInventory() {
+	public static Inventory myInventory() {
 
-        // Create our inventory-
-        Inventory myInventory = Bukkit.createInventory(null, 18, "Colors!");
+		// Create our inventory-
+		Inventory myInventory = Bukkit.createInventory(null, 18, "Colors!");
 
-        return myInventory;
-    }
+		return myInventory;
+	}
 
-    public static void setBroadcastCount(int count) {
-        broadcastCounter += count;
+	public static void setBroadcastCount(int count) {
+		broadcastCounter += count;
 
-    }
+	}
 
-    public static int getBroadcastCount() {
-        return broadcastCounter;
+	public static int getBroadcastCount() {
+		return broadcastCounter;
 
-    }
+	}
 
-    public static void broadcast() {
-        Main.getInstance().getServer().broadcastMessage(
-                ChatColor.translateAlternateColorCodes('&', "&b&l/gamble is drawing VERY SOON! Buy your tickets now!"));
+	public static void resetBroadcastCount() {
+		broadcastCounter = 0;
 
-    }
+	}
 
-    public static String draw() {
+	public static void broadcast() {
+		setBroadcastCount(1);
+		Bukkit.getServer().broadcastMessage("");
+		Main.getInstance().getServer().broadcastMessage(
+				ChatColor.translateAlternateColorCodes('&', "&b&l/gamble is drawing VERY SOON! Buy your tickets now!"));
+		Bukkit.getServer().broadcastMessage("");
+	}
 
-        String winner = "";
+	public static String draw() {
 
-        int probability = (int) (Math.random() * (100)) + 1;
+		String winner = "";
 
-        if (probability > 0 && probability < 11) {
-            winner = "red";
+		int probability = (int) (Math.random() * (100)) + 1;
 
-        } else if (probability > 11 && probability < 31) {
-            winner = "black";
-        } else if (probability > 31 && probability < 61) {
-            winner = "green";
+		if (probability > 0 && probability < 11) {
+			winner = "red";
 
-        } else if (probability > 61 && probability < 101) {
-            winner = "blue";
-        }
+		} else if (probability > 11 && probability < 31) {
+			winner = "black";
+		} else if (probability > 31 && probability < 61) {
+			winner = "green";
 
+		} else if (probability > 61 && probability < 101) {
+			winner = "blue";
+		}
 
+		return winner;
+	}
 
+	public static void setCachedWinnner(String color) {
+		cachedColor = color;
 
-        return winner;
-    }
+	}
 
-    public static void winnerPayOut(String winningColor) {
-        for(Player player : Bukkit.getServer().getOnlinePlayers()) {
-            String ticketRed = getTicketHash().get(player.getDisplayName().toString()).split("-")[0];
-            String ticketBlack = main.Main.playerTickets.get(player.getDisplayName().toString()).split("-")[1];
-            String ticketGreen = main.Main.playerTickets.get(player.getDisplayName().toString()).split("-")[2];
+	public static String whatIsCachedWinner() {
+		return cachedColor;
 
-            Main.econ.bankDeposit(player.getName().toString(), );
+	}
 
+	public static void clearCache() {
+		setCachedWinnner("");
 
-        }
+	}
 
-    }
+	public static void newRound() {
+		clearCache();
 
-    }
+	}
+
+	public static void messsagePayout(String player, int payoutAmount) {
+		String payoutMessage = "Hey, you just won: " + String.valueOf(payoutAmount);
+
+		Bukkit.getPlayer(player).sendMessage(payoutMessage);
+
+		// TODO: Message the player.
+	}
+
+	public static void winnerPayOut(String winningColor) {
+		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+			if (getTicketHash().containsKey(player.getName() != null)) {
+				int multiplier = 1;
+				int winningMoney = 0;
+				String ticketRed = getTicketHash().get(player.getDisplayName().toString()).split("-")[0];
+				String ticketBlack = main.Main.playerTickets.get(player.getDisplayName().toString()).split("-")[1];
+				String ticketGreen = main.Main.playerTickets.get(player.getDisplayName().toString()).split("-")[2];
+
+				if (winningColor == "red") {
+					multiplier = Integer.getInteger(ticketRed);
+					winningMoney = (int) ((multiplier * 20000) * .1);
+
+				} else if (winningColor == "black") {
+
+					multiplier = Integer.getInteger(ticketBlack);
+					winningMoney = (int) ((multiplier * 20000) * .2);
+
+				} else if (winningColor == "green") {
+					multiplier = Integer.getInteger(ticketGreen);
+					winningMoney = (int) ((multiplier * 20000) * .4);
+
+				}
+
+				Main.econ.bankDeposit(player.getName().toString(), winningMoney);
+				messsagePayout(player.getName().toString(), winningMoney);
+
+			}
+		}
+	}
 
 }
-
-
--- CityGamble/src/main/Listeners.java
+++ CityGamble/src/main/Listeners.java
@@ -9,52 +9,56 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class Listeners {
 
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        Player player = (Player) event.getWhoClicked();
-        ItemStack clicked = event.getCurrentItem();
-        Inventory inventory = event.getInventory();
-        // Check if it's our inventory
-        if (inventory.getName().equals(API.myInventory().getName())) {
-            // Check if the player is null, if so we'll set him with the
-            // defeault setup.
-            if (Main.playerTickets.get(player.getName()) == null) {
-                Main.playerTickets.put(player.getName(), gamble.ticketSetup);
-            }
-            // Cancel the event
-            event.setCancelled(true);
-            if (clicked.equals(API.myInventory().getItem(3))) {
-                // slot number 3 is red.
-                ticketCount.ticketCounter("red", player.getName());
-            } else if (clicked.equals(API.myInventory().getItem(5))) {
-                // slot 5 is black
-                ticketCount.ticketCounter("black", player.getName());
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent event) {
+		Player player = (Player) event.getWhoClicked();
+		ItemStack clicked = event.getCurrentItem();
+		Inventory inventory = event.getInventory();
+		// Check if it's our inventory
+		if (inventory.getName().equals(API.myInventory().getName())) {
+			// Cancel the event
+			event.setCancelled(true);
 
-            } else if (clicked.equals(API.myInventory().getItem(7))) {
-                // slot 7 is green
-                ticketCount.ticketCounter("green", player.getName());
-            }
+			// Check if the player is null, if so we'll set him with the
+			// defeault setup.
+			if (Main.playerTickets.get(player.getName()) == null) {
+				Main.playerTickets.put(player.getName(), gamble.ticketSetup);
+			}
+			if (clicked.equals(API.myInventory().getItem(3))) {
+				// slot number 3 is red.
+				ticketCount.ticketCounter("red", player.getName());
+			} else if (clicked.equals(API.myInventory().getItem(5))) {
+				// slot 5 is black
+				ticketCount.ticketCounter("black", player.getName());
 
-            // TODO: Call something that'll check if x tickets sold and if so
-            // we'll start a runabble
-            // to start a countdown to launch and pick the winners.
+			} else if (clicked.equals(API.myInventory().getItem(7))) {
+				// slot 7 is green
+				ticketCount.ticketCounter("green", player.getName());
+			}
 
-            new BukkitRunnable() {
-                public void run() {
-                    if (API.ticketsSold() > 2 && API.getBroadcastCount() < 3) {
-                        API.setBroadcastCount(1);
-                        API.broadcast();
-                    }
-                    if (API.getBroadcastCount() > 3) {
-                        String winner = API.draw();
-                        API.messageWinners(winner);
-                        API.payWinners();
-                    }
+		}
 
-                }
-            }.runTaskTimer(Main.getInstance(), 1200l, 1200l);
+		// TODO: Call something that'll check if x tickets sold and if so
+		// we'll start a runabble
+		// to start a countdown to launch and pick the winners.
 
-        }
-        // playerTickets.put(player.getName(), "Red-" + ticketsbought);
-    }
+		new BukkitRunnable() {
+			public void run() {
+				if (API.ticketsSold() > 2 && API.getBroadcastCount() < 3) {
+					System.out.println("YOLO");
+					API.setBroadcastCount(1);
+					API.broadcast();
+				}
+				if (API.getBroadcastCount() > 3 && API.ticketsSold() > 2) {
+					String winner = API.draw();
+					API.setCachedWinnner(winner);
+					API.winnerPayOut(API.whatIsCachedWinner());
+					API.newRound();
+				}
+
+			}
+		}.runTaskTimer(Main.getInstance(), 1200l, 600L);
+
+	}
+	// playerTickets.put(player.getName(), "Red-" + ticketsbought);
 }
